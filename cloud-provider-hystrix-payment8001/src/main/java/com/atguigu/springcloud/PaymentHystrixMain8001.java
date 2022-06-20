84fa4be8a9508203f873d3bfddd281db1a9e8275
ndq@qq.com <ndq@qq.com>
2022-06-20 22:58:57 +0800

    [ndq]20220620 hot-fix 1 Commit!

--- cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/PaymentHystrixMain8001.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/PaymentHystrixMain8001.java
@@ -22,6 +22,7 @@ public class PaymentHystrixMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentHystrixMain8001.class,args);
         System.out.println("hello git");
+        System.out.println("hello git1");
     }
 
 
